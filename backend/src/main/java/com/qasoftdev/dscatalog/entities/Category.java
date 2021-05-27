b36ed9c682969f4066b71e507f51b1f3f32932f3
Rodrigo Acevedo <rodrigoacevedo@outlook.com>
2021-05-27 18:35:32 -0300

    Pagination

--- backend/src/main/java/com/qasoftdev/dscatalog/entities/Category.java
+++ backend/src/main/java/com/qasoftdev/dscatalog/entities/Category.java
@@ -22,6 +22,7 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant	createdAt;
 	
